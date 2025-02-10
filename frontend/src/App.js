8d809204d1a093365deb7e1c75aa4101c9a07893
Georgia <georgiakoda@outlook.com>
2025-02-10 13:29:56 -0500

    added basic/unstyled/not-functional search bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Sentiment Analysis Tool</h1>
+        <h4> Enter a search term below: </h4>
+        <SearchBar />
+        
       </header>
+    
+      <div>
+        
+      </div>
+
     </div>
   );
 }
---
+++ frontend/src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function SearchBar() {
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="Search..."
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+}
+
+export default SearchBar;
