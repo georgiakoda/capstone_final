271aa71bb6d67f42a54e29a9334a00168e06ac1b
Georgia <georgiakoda@outlook.com>
2025-03-10 11:31:10 -0400

    search results are displayed in the console

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -8,6 +8,7 @@ import React, { useState } from "react";
 function SearchBar({ onSearch }) {
   const [query, setQuery] = useState("");
 
+
   //Handle form submission
   const handleSubmit = async (e) => { //async because need to use await
     e.preventDefault();
@@ -24,6 +25,19 @@ function SearchBar({ onSearch }) {
 
         const result = await response.json();
         console.log("Keyword added:", result);
+
+        // Uses the Reddit API endpoint to search for the entered term
+        const redditResponse = await fetch(`http://localhost:8000/search/reddit/?q=${encodeURIComponent(query)}`);
+        const redditResult = await redditResponse.json();
+
+        // Right now it just prints the results in the console
+        // It takes a few seconds to display the results so don't keep pressing the button if it doesn't show up right away
+        console.log("Reddit Search Results:", redditResult.results);
+        console.log("Rate Limit Info:", redditResult.rate_limit_info);
+
+
+
+
       } catch (error) {
         console.error("Error submitting keyword:", error);
       }
