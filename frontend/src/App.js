3973bb14e07f3a52c7dceace211d699ed501162a
Georgia <georgiakoda@outlook.com>
2025-05-05 11:33:56 -0400

    moved home page code to separate component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import SearchBar from './components/SearchBar';
+import Home from './components/Home';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import HowTo from './components/HowTo';
@@ -11,33 +12,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
-  const handleSearch = (query) => {
-    console.log("searched term: ", query);
-  };
-
   return (
     <Router>
     <div className="App">
       <NavBar />
       <Routes>
-  <Route path="/" element={
-    <header className="App-header">
-      <div className="container my-5">
-        <div className="p-5 text-center bg-body-tertiary rounded-3">
-          <div className="p-3 p-lg-5 pt-lg-3">
-            <h1 className="display-4 fw-bold lh-1 text-body-emphasis">
-              Reddit Sentiment Analyzer
-            </h1>
-            <p className="lead text-dark">
-              Enter a keyword below to search for Reddit posts. 
-            </p>
-            <SearchBar onSearch={handleSearch} />
-          </div>
-        </div>
-      </div>
-    </header>
-  } />
-
+  <Route path="/" element={<Home />} />
   <Route path="/howto" element={<HowTo />} />
   <Route path="/about" element={<About />} />
   <Route path="/history" element={<KeywordHistory />} />
